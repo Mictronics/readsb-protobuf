05dade805c69af95230e21107aa4af770f8d4e65
Mictronics <michael@mictronics.de>
2020-02-01 14:37:08 +0100

    Reads history protocol buffer files using mapbox/pbf.

---
+++ webapp/src/script/readsb/backend/aircraftHistory.js
@@ -0,0 +1,87 @@
+"use strict";
+self.importScripts("../../pbf.js");
+self.importScripts("readsb-pb.js");
+var READSB;
+(function (READSB) {
+    let AircraftTraceCollector = null;
+    const PositionHistoryBuffer = [];
+    self.onmessage = (ev) => {
+        const msg = ev.data;
+        switch (msg.type) {
+            case "Port":
+                AircraftTraceCollector = msg.data;
+                AircraftTraceCollector.onmessage = (evt) => {
+                    console.info(`TraceCollector: ${evt.data}`);
+                };
+                break;
+            case "HistorySize":
+                StartLoadHistory(msg.data);
+                break;
+            default:
+                break;
+        }
+    };
+    function StartLoadHistory(historySize) {
+        let loaded = 0;
+        if (historySize > 0) {
+            for (let i = 0; i < historySize; i++) {
+                fetch(`http://192.168.178.25/radar/data/history_${i}.pb`, {
+                    cache: "no-cache",
+                    method: "GET",
+                    mode: "cors",
+                })
+                    .then((res) => {
+                    loaded++;
+                    if (res.status >= 200 && res.status < 300) {
+                        return Promise.resolve(res);
+                    }
+                    else {
+                        return Promise.reject(res.statusText);
+                    }
+                })
+                    .then((res) => {
+                    return res.arrayBuffer();
+                }, (res) => {
+                    loaded++;
+                    console.error(`Failed to load history chunk: ${res.message}`);
+                })
+                    .then((pb) => {
+                    if (loaded < 0) {
+                        return;
+                    }
+                    const pbf = new Pbf(pb);
+                    const data = READSB.AircraftsUpdate.read(pbf);
+                    pbf.destroy();
+                    PositionHistoryBuffer.push(data);
+                    if (loaded >= historySize) {
+                        loaded = -1;
+                        DoneLoadHistory();
+                    }
+                })
+                    .catch((error) => {
+                    if (loaded < 0) {
+                        return;
+                    }
+                    console.error(error.stack);
+                    self.close();
+                });
+            }
+        }
+    }
+    function DoneLoadHistory() {
+        if (PositionHistoryBuffer.length > 0) {
+            PositionHistoryBuffer.sort((x, y) => x.now - y.now);
+            for (const h of PositionHistoryBuffer) {
+                h.aircraft.forEach((ac, i) => {
+                    if ((ac.lat !== null) && (ac.lon !== null) && (ac.alt_baro !== null)) {
+                        const pos = new Array(ac.lat, ac.lon, ac.alt_baro);
+                        const msg = { type: "Update", data: [ac.addr, pos, h.now] };
+                        AircraftTraceCollector.postMessage(msg);
+                    }
+                });
+            }
+        }
+        self.close();
+    }
+})(READSB || (READSB = {}));
+//# sourceMappingURL=aircraftHistory.js.map
---
+++ webapp/src/script/readsb/backend/aircraftHistory.ts
@@ -0,0 +1,119 @@
+// Part of readsb, a Mode-S/ADSB/TIS message decoder.
+//
+// aircraftHistory.ts: Aircraft history background worker.
+//
+// Copyright (c) 2020 Michael Wolf <michael@mictronics.de>
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This file is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+self.importScripts("../../pbf.js");
+self.importScripts("readsb-pb.js");
+
+namespace READSB {
+    let AircraftTraceCollector: MessagePort = null; // Message port to trace collector worker
+    const PositionHistoryBuffer: IAircraftsUpdate[] = [];
+
+    /**
+     * Handle incoming messages from web frontend or trace collector worker.
+     */
+    self.onmessage = (ev: MessageEvent) => {
+        const msg = ev.data;
+        switch (msg.type) {
+            case "Port":
+                AircraftTraceCollector = msg.data;
+                AircraftTraceCollector.onmessage = (evt: MessageEvent) => {
+                    console.info(`TraceCollector: ${evt.data}`);
+                };
+                break;
+            case "HistorySize":
+                StartLoadHistory(msg.data);
+                break;
+            default:
+                break;
+        }
+    };
+
+    /**
+     * Start loading aircraft history from readsb backend.
+     * @param historySize Size of aircraft history.
+     */
+    function StartLoadHistory(historySize: number) {
+        let loaded = 0;
+        if (historySize > 0) {
+            for (let i = 0; i < historySize; i++) {
+                fetch(`http://192.168.178.25/radar/data/history_${i}.pb`, {
+                    cache: "no-cache",
+                    method: "GET",
+                    mode: "cors",
+                })
+                    .then((res: Response) => {
+                        loaded++;
+                        if (res.status >= 200 && res.status < 300) {
+                            return Promise.resolve(res);
+                        } else {
+                            return Promise.reject(res.statusText);
+                        }
+                    })
+                    .then((res: Response) => {
+                        return res.arrayBuffer();
+                    }, (res: any) => {
+                        loaded++;
+                        console.error(`Failed to load history chunk: ${res.message}`);
+                    })
+                    .then((pb: ArrayBuffer) => {
+                        if (loaded < 0) {
+                            return;
+                        }
+                        const pbf = new Pbf(pb);
+                        const data = AircraftsUpdate.read(pbf);
+                        pbf.destroy();
+                        PositionHistoryBuffer.push(data); // don't care for order, will sort later
+                        if (loaded >= historySize) {
+                            loaded = -1;
+                            DoneLoadHistory();
+                        }
+                    })
+                    .catch((error) => {
+                        if (loaded < 0) {
+                            return;
+                        }
+                        console.error(error.stack);
+                        self.close();
+                    });
+            }
+        }
+    }
+
+    /**
+     * Forward history data to aircraft trace collector.
+     */
+    function DoneLoadHistory() {
+        if (PositionHistoryBuffer.length > 0) {
+            // Sort history by timestamp
+            PositionHistoryBuffer.sort((x, y) => x.now - y.now);
+            // Process history
+            for (const h of PositionHistoryBuffer) {
+                h.aircraft.forEach((ac: IAircraftMeta, i: number) => {
+                    if ((ac.lat !== null) && (ac.lon !== null) && (ac.alt_baro !== null)) {
+                        const pos = new Array(ac.lat, ac.lon, ac.alt_baro);
+                        const msg = { type: "Update", data: [ac.addr, pos, h.now] };
+                        AircraftTraceCollector.postMessage(msg);
+                    }
+                });
+            }
+        }
+        // Job done, self terminated.
+        self.close();
+    }
+}
