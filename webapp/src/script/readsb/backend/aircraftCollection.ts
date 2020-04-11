610e7c1a1a5e9526e133136de018c1f931b1076b
Christian Ress <bart@github.the-bart.org>
2020-04-11 15:17:15 +0200

    Add polyfill for nested dedicated workers as WebKit does not support them.

--- webapp/src/script/readsb/backend/aircraftCollection.ts
+++ webapp/src/script/readsb/backend/aircraftCollection.ts
@@ -18,6 +18,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 self.importScripts("../../pbf.js");
+self.importScripts("../../subworkers.js");
 self.importScripts("../enums.js");
 self.importScripts("database.js");
 self.importScripts("registration.js");
